e19cac92d3ade475a110d931c215346658e5b9a1
robstowe <robstowe94@gmail.com>
2023-07-23 13:47:46 -0700

    adding pathing details to app.js and index.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,37 @@
-+
+import React from 'react';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Contact from './components/Contact';
+import About from './components/About';
+import Resume from './components/Resume';
+import Home from './components/Home';
+import Nav from './components/Nav';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default function App() {
+    return (
+        <Router>
+        <Nav />
+        <Routes>
+            <Route
+                path="/"
+                element={<Home />}/>
+            <Route 
+                path="/about"
+                element={<About />}/>
+            <Route
+                path="/projects"
+                element={<Projects />}/>
+            <Route
+                path="/contact"
+                element={<Contact />}/>
+            <Route
+                path="/resume"
+                element={<Resume />}/>
+        </Routes>
+        </Router>
+    )
+}
