7d630abfe6a227573afbba2f1c1c0b6f30890249
robstowe <robstowe94@gmail.com>
2023-07-26 12:41:36 -0700

    fixed logo positioning

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -7,17 +7,16 @@ import Bob from './assets/robstowe.png'
 function daNavBar() {
     return (
         <div>
-            <navbar href='/'>
-            <img src={Bob} alt="Rob Stowe logo" className="navbar-logo"/>
-            </navbar>
-                {/* <Container fluid> */}
-                    <nav className="parent">
-                        <a href='/About' className="navlink" variant='link'>About</a>
-                        <a href='/Projects' className="navlink" variant='link'>Projects</a>
-                        <a href='/Resume' className="navlink" variant='link'>Resume</a>
-                        <a href='/Contact' className="navlink" variant='link'>Contact</a>
-                    </nav>
-                {/* </Container> */}
+
+            <nav className="parent">
+                <navbar href='/'>
+                    <img src={Bob} alt="Rob Stowe logo" className="navbar-logo" />
+                </navbar>
+                <a href='/About' className="navlink" variant='link'>About</a>
+                <a href='/Projects' className="navlink" variant='link'>Projects</a>
+                <a href='/Resume' className="navlink" variant='link'>Resume</a>
+                <a href='/Contact' className="navlink" variant='link'>Contact</a>
+            </nav>
         </div>
     )
 }
