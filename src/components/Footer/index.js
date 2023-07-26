55faa231d7ef11a1b4d438face1dade2b8fbdc8b
robstowe <robstowe94@gmail.com>
2023-07-26 08:45:32 -0700

    fixed type writer and added styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './style.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { Github, Envelope, Linkedin } from 'react-bootstrap-icons'
 
 
 export default function Footer(){
@@ -9,11 +10,14 @@ export default function Footer(){
         <footer className="footer-container">
             <div className="footer-container">
                 <div className="icon-content">
-                    <a href="https://www.linkedin.com/in/rob-stowe/" target="_blank" rel="nopener no referrer">
-                        <FontAwesomeIcon icon={faLinkedin} />
+                    <a href="https://www.linkedin.com/in/rob-stowe/">
+                        <Linkedin />
                     </a>
-                    <a href="https://github.com/robstowe" target="_blank" rel="nopener no referrer">
-                        <FontAwesomeIcon icon={faGithub} />
+                    <a href="mailto:robstowe94@gmail.com">
+                        <Envelope/>
+                    </a>
+                    <a href="https://github.com/robstowe" target="_blank">
+                        <Github />
                     </a>
                 </div>
             </div>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,22 +6,20 @@ import { Container } from 'react-bootstrap';
 
 export default function Home() {
     return (
-        <div className = "content row">
+        <div className = "content-row">
             <Container className="d-flex justify-content-center align-items-center">
             <div className="text-center typewriter">
                 <Typewriter 
                 onInit={(typewriter) => {
                     typewriter
                     .changeDelay(50)
-                    .typeString("Hey! I'm Rob.")
+                    .typeString("Hey! I'm Rob. ")
                     .pauseFor(1000)
-
-                    .typeString("I'm a Full Stack Developer and recent graduate from North Western University")
+                    .typeString("<br>I'm a Full Stack Developer and recent graduate from North Western University. ")
                     .pauseFor(1000)
-
-                    .typeString("Please, take a look around my portfolio!")
+                    .typeString("<br>Please, take a look around my portfolio...")
                     .pauseFor(1000)
-
+                    .start();
                 }}/>
             </div>
             </Container>
